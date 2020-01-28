0bb0f2f5b6f5b680535f158d53777508620f6638
Evgeny Karasev <gnomskg@gmail.com>
2020-01-28 18:35:09 +0300

    Change backend platform from feather to native express

--- server/test/services/article-metadata.test.js
+++
@@ -1,8 +0,0 @@
-const app = require('../../src/app');
-
-describe("'article-metadata' service", () => {
-  it('registered the service', () => {
-    const service = app.service('article-metadata');
-    expect(service).toBeTruthy();
-  });
-});
