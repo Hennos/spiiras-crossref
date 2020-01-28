79a0aa7f2a9a4fbf0d0b40e7cf0ef83d86c1adc2
Evgeny Karasev <gnomskg@gmail.com>
2020-01-28 18:43:04 +0300

    Add formatter and prettier, fix code style

--- server/app.js
+++ server/app.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const cors = require("cors");
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-const indexRouter = require("./routes/index");
+const indexRouter = require('./routes/index');
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/", indexRouter);
+app.use('/', indexRouter);
 
 module.exports = app;
